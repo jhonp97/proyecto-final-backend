7b1322dc289fb735afddf9d0956bf52734f2ebb4
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-07-16 13:54:35 +0200

    reseñas check

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -33,7 +33,46 @@ export const ObtenerReseñas = async (req, res, next) => {
 }
 
 export const ActualizarReseña = async (req, res, next) => {
+     try {
+    const { reviewId } = req.params;
+    const { rating, comment } = req.body;
+
+    const userId = req.userId;
+    const review = await Review.findById(reviewId);
+    if (!review) {
+      return res.status(404).json({ msg: "Reseña no encontrada." });
+    }
+    //se verifica que el usuario que actualiza es el autor de la reseña para que sea aun mas seguro
+    if (review.user.toString() !== userId) {
+      return res.status(403).json({ msg: "No autorizado para editar esta reseña." });
+    }
+    review.rating = rating || review.rating;
+    review.comment = comment || review.comment;
+    const updatedReview = await review.save();
+    res.status(200).json(updatedReview);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export const EliminarReseña = async (req, res, next) => {
+    try {
+    const { reviewId } = req.params;
+    const userId = req.userId;
+
+    const review = await Review.findById(reviewId);
+    if (!review) {
+      return res.status(404).json({ msg: "Reseña no encontrada." });
+    }
+    //se verifica que el usuario que actualiza es el autor de la reseña para que sea aun mas seguro
+    if (review.user.toString() !== userId) {
+      return res.status(403).json({ msg: "No autorizado para editar esta reseña." });
+    }
+    await review.deleteOne()
+    //se elimina la referencua del array del usuario
+    await User.findByIdAndUpdate(userId, { $pull: { reseñas: reviewId } });
+    res.status(200).json({msg: "Reseña eliminada correctamente." });
+  } catch (error) {
+    next(error);
+  }
 }
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,7 +7,7 @@ import { agregarFavorito, eliminarFavorito, obtenerFavoritos } from "../controll
 import {registerUser,loginUser,getCurrentUser} from "../controllers/auth.controller.js";
 import { updatePerfil } from "../controllers/user.controller.js";
 import upload from "../middleware/upload.middleware.js";
-import { CrearReseña, ObtenerReseñas } from "../controllers/review.controller.js";
+import { ActualizarReseña, CrearReseña, EliminarReseña, ObtenerReseñas } from "../controllers/review.controller.js";
 
 
 const router = express.Router();
@@ -28,8 +28,12 @@ router.post("/favoritos", authMiddleware, agregarFavorito);
  router.put("/perfil", authMiddleware,upload.single("fotoPerfil"), updatePerfil);
  router.get("/favoritos", authMiddleware, obtenerFavoritos);
 router.delete("/favoritos/:animeId", authMiddleware, eliminarFavorito)
+
+// RESEÑAS
 router.post('/reviews', authMiddleware, CrearReseña);
 router.get('/reviews/:animeId', ObtenerReseñas);
+router.put('/reviews/:reviewId', authMiddleware, ActualizarReseña);
+router.delete('/reviews/:reviewId', authMiddleware, EliminarReseña);
 
 // RUTAS PÚBLICAS
 
